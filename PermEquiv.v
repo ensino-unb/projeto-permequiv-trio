2a968636d7428cb8f233becde5befae0b0f960c2
Marcelo <marcelojunqueiraf@gmail.com>
2021-10-06 22:44:27 -0300

    versão quase final

--- PermEquiv.v
+++ PermEquiv.v
@@ -2,6 +2,14 @@ Require Import PeanoNat List.
 
 Open Scope nat_scope.
 
+
+Fixpoint insere (n:nat) (l: list nat) :=
+  match l with
+  | nil => n :: nil (* lista unitária *)
+  | h :: tl => if n <=? h then (n :: l) (* inserção de n na primeira posição *)
+             else (h :: (insere n tl)) (* inserção de n na cauda da lista *)
+  end.
+
 Fixpoint num_oc n l  :=
   match l with
     | nil => 0
@@ -57,8 +65,132 @@ Proof.
 Qed.
 
 
+(*permutation*)
 Definition permutation l l' := forall n:nat, num_oc n l = num_oc n l'.
 
+Lemma permutation_refl: forall l, permutation l l.
+Proof.
+  intro l.
+  unfold permutation.
+  intro x.
+  reflexivity.
+Qed.
+
+Lemma permutation_nil: forall l, permutation nil l -> l = nil.
+Proof.
+  intro l.
+  case l.
+  - intro H.
+    reflexivity.
+  - intros n l' H.
+    unfold permutation in H.
+    specialize (H n).
+    simpl in H.
+    rewrite Nat.eqb_refl in H.
+    inversion H.
+Qed.
+
+Lemma permutation_sym: forall l l', permutation l l' -> permutation l' l.
+Proof.
+  intros l l' H.
+  unfold permutation in *.
+  intro n.
+  apply eq_sym.
+  apply H.
+Qed.
+
+Lemma permutation_trans: forall l1 l2 l3, permutation l1 l2 -> permutation l2 l3 -> permutation l1 l3.
+Proof.
+  intros.
+  induction l1.
+  -apply permutation_nil in H.
+   rewrite H in H0.
+   assumption. 
+  -unfold permutation in *.
+   simpl in *.
+   intros n.
+   assert (H := H n).
+   destruct (n =? a) in *.
+   + rewrite H.
+     apply H0.
+   + rewrite H.
+     apply H0.
+Qed.
+
+Lemma permutation_hd: forall l l' x, permutation l l' -> permutation (x :: l) (x :: l').
+Proof.
+  intros l l' x H.
+  unfold permutation in *.
+  intro x'.
+  destruct (x'=?x) eqn:H'.
+  -simpl.
+    rewrite H'.
+    rewrite H.
+    reflexivity.
+  -simpl.
+    rewrite H'.
+    rewrite H.
+    reflexivity.
+Qed.
+
+Lemma permutation_hd_back: forall l l' x, permutation (x::l) (x::l') -> permutation l l'.
+Proof.
+  intros l l' x H.
+  unfold permutation in *. 
+  intro x'.
+  specialize (H x').
+  simpl in H.
+  destruct (x' =? x)eqn: J.
+  +  apply eq_add_S in H.
+    assumption.
+  + assumption.
+Qed.
+
+Lemma permutation_2head: forall l x y, permutation ( x::y::l)(y::x::l).
+Proof.
+  intros l x y.
+  unfold permutation.
+  intro x0.
+  simpl.
+  destruct (x0 =? x) eqn:H.
+  +destruct(x0 =? y) eqn:J.
+    -reflexivity.
+    -reflexivity.
+  +destruct(x0 =? y) eqn:J.
+    -reflexivity.
+    -reflexivity.
+Qed.
+
+Lemma permutation_insere: forall l l' x, permutation l l' -> permutation (x :: l) (insere x l').
+Proof.
+  intros l l' x H.
+  simpl.
+  generalize dependent x.
+  generalize dependent l.
+  induction l'.
+  - intros l H x.
+    apply permutation_sym in H.
+    apply permutation_nil in H.
+    rewrite H.
+    simpl.
+    apply permutation_refl.
+  - intros l H x.
+    apply permutation_trans with (x :: a :: l').
+    +apply permutation_hd.
+      assumption.
+    +simpl.
+      destruct (x <=? a) eqn:J.
+      *apply permutation_refl.
+      *apply permutation_trans with (a :: x :: l').
+        ** unfold permutation in *.
+           apply permutation_2head.
+        **apply permutation_hd.
+           apply IHl'.
+           apply permutation_refl.
+Qed.
+
+
+(*perm*)
 Inductive perm: list nat -> list nat -> Prop :=
 | perm_refl: forall l, perm l l
 | perm_hd: forall x l l', perm l l' -> perm (x::l) (x::l')
@@ -79,25 +211,40 @@ Proof.
     + apply perm_hd.
       apply IHl1.
 Qed.
-      
+ Lemma perm_insere: forall l x, perm (x :: l) (insere x l).
+ Proof.
+   induction l.
+   - intro x.
+     simpl.
+     apply perm_refl.
+   - intro x.
+     simpl.
+     destruct (x <=? a).
+     + apply perm_refl.
+     + apply perm_trans with (a :: x :: l).
+       * apply perm_swap.
+         apply perm_refl.
+       * apply perm_hd.
+         apply IHl.
+Qed.      
+
+
+
+
 (** Questão 1 *)
 Lemma perm_to_permutation: forall l l', perm l l' -> permutation l l'.
-Proof.
-Admitted.
+  induction 1.
+   -apply permutation_refl.
+   -apply permutation_hd. apply IHperm.
+   -apply permutation_trans with (y::x::l).
+     +apply permutation_2head.
+     +apply permutation_hd. apply permutation_hd. apply IHperm.
+   -apply permutation_trans with l2.
+     +assumption.
+     +assumption.
+Qed.
 
 (** Questão 2 *)
-Lemma permutation_nil: forall l, permutation nil l -> l = nil.
-Proof.
-Admitted.
-
-Lemma permutation_sym: forall l l', permutation l l' -> permutation l' l.
-Proof.
-  intros l l' H.
-  unfold permutation in *.
-  intro n.
-  apply eq_sym.
-  apply H.
-Qed.
 
 Lemma permutation_cons: forall n l l', permutation (n :: l) (n :: l') <-> permutation l l'.
 Proof.
@@ -124,24 +271,6 @@ Proof.
     + apply H.
 Qed.
 
-Lemma permutation_trans: forall l1 l2 l3, permutation l1 l2 -> permutation l2 l3 -> permutation l1 l3.
-Proof.
-  intros.
-  induction l1.
-  -apply permutation_nil in H.
-   rewrite H in H0.
-   assumption. 
-  -unfold permutation in *.
-   simpl in *.
-   intros n.
-   assert (H := H n).
-   destruct (n =? a) in *.
-   + rewrite H.
-     apply H0.
-   + rewrite H.
-     apply H0.
-Qed.
-
 Lemma permutation_comm_app: forall l1 l2, permutation (l1 ++ l2) (l2 ++ l1).
 Proof.
   intros l1 l2.
@@ -153,6 +282,13 @@ Qed.
 Lemma permutation_app_cons: forall l1 l2 a, permutation (l1 ++ a :: l2) (a :: l1 ++ l2).
 Proof.
   induction l1.
+  - intros.
+    unfold permutation.
+    intros.
+    rewrite app_nil_l.
+    rewrite app_nil_l.
+    reflexivity.
+  - admit.
 Admitted.
   
 Lemma permutation_cons_num_oc: forall n l l', permutation (n :: l) l' -> exists x, num_oc  n l' = S x.
@@ -173,15 +309,40 @@ Proof.
 Admitted.
 
 (** Questão 4 *)
-Lemma permutation_to_perm: forall l l', permutation l l' -> perm l l'.
+Lemma perm_implies_Permutation: forall l l', perm l l' -> permutation l l'.
 Proof.
- induction l.
- - admit.
- - intros l' Hequiv.
-   generalize dependent a.
-   generalize dependent l.
-   case l'.
-   + Admitted.
+  induction 1.
+  - apply permutation_refl.
+  - apply permutation_hd. assumption.
+  - apply permutation_trans with (y :: x :: l).
+    + apply permutation_2head.
+    + repeat apply permutation_hd.
+      apply IHperm.
+  - apply permutation_trans with l2; assumption.
+Qed.
+
+
+
+Lemma permutation_to_perm: forall l l', permutation l l' -> perm l l'.
+  induction l.
+   -intros l' H.
+     apply permutation_nil in H. rewrite H. apply perm_refl.
+   -intro l'.
+     case l'.
+     +intro H.
+       apply permutation_sym in H. apply permutation_nil in H. rewrite H.
+       apply perm_refl.
+     +intros n l0 H.
+      case (a=?n) eqn:IHG.
+      --apply Nat.eqb_eq in IHG.
+        rewrite IHG in *.
+        apply permutation_hd_back in H.
+        apply perm_hd.
+        apply IHl.
+        assumption.
+      --apply Nat.eqb_neq in IHG.
+
+        
 
 Theorem perm_equiv: forall l l', perm l l' <-> permutation l l'.
 Proof.
@@ -190,4 +351,3 @@ Proof.
   - apply perm_to_permutation.
   - apply permutation_to_perm.
 Qed.
-
